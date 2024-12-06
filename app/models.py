7907296e827c65eeb625549b86fbbf93a313ff11
PorshaDP <114876431+PorshaDP@users.noreply.github.com>
2024-12-06 16:07:29 +0300

    upd_back+front
    Reworked fragments of code and project's structure

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.context import CryptContext
 from sqlalchemy import create_engine
+import csv
+import random
 
 Base = declarative_base()
 
@@ -12,7 +14,7 @@ class User(Base):
     name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    photo_path = Column(Text, nullable=True)  # Новое поле для пути к фото
+    photo_path = Column(Text, nullable=True)  
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -38,3 +40,43 @@ def get_user_by_email(db: Session, email: str):
 
 def get_user_by_name(db: Session, name: str):
     return db.query(User).filter(User.name == name).first()
+
+def get_random_book():
+
+    try:
+        with open("app/learning/filtered_books.csv", encoding="utf-8") as csvfile:
+            reader = list(csv.DictReader(csvfile))
+            book = random.choice(reader)
+            return {
+                "title": book.get("Title", "Не указано"),
+                "authors": book.get("authors", "Не указано"),
+                "published_year": book.get("published_year", "Не указано"),
+                "categories": book.get("categories", "Не указано"),
+                "thumbnail": book.get("thumbnail", ""),
+                "description": book.get("description", "Описание отсутствует"),
+                "average_rating": book.get("average_rating", "Нет рейтинга"),
+                "num_pages": book.get("num_pages", "Нет данных о страницах"),
+            }
+    except FileNotFoundError:
+        return {"error": "Файл с книгами не найден"}
+    except Exception as e:
+        return {"error": f"Произошла ошибка: {str(e)}"}
+
+
+def get_random_movie():
+
+    try:
+        with open("app/learning/filtered_movies.csv", encoding="utf-8") as csvfile:
+            reader = list(csv.DictReader(csvfile))
+            movie = random.choice(reader)
+            return {
+                "title": movie.get("title", "Не указано"),
+                "genre": movie.get("Genre", "Не указано"),
+                "year": movie.get("Year", "Не указано"),
+                "score": movie.get("Score", "Нет оценки"),
+                "description": movie.get("Description", "Описание отсутствует"),
+            }
+    except FileNotFoundError:
+        return {"error": "Файл с фильмами не найден"}
+    except Exception as e:
+        return {"error": f"Произошла ошибка: {str(e)}"}
